b914bba76a39ed7a4bb916a15526e12db7a311fb
Mikhail <lexxx2@mail.ru>
2016-05-26 16:41:32 -0400

    module1-task2-update

--- build/js/check.js
+++ build/js/check.js
@@ -1,32 +1,33 @@
 function getMessage(a, b) {
 
-    if (typeof(a) === 'boolean') {
-        return 'Я попал в ' + b;
-    }    
-        
-    else {
-        return 'Я никуда не попал';
+  if (typeof(a) === 'boolean') {
+    if(a) {
+      return 'Я попал в ' + b;
     }
+      else {
+          return 'Я никуда не попал';
+      }
+  }
 
-    if (typeof(a) === 'number') {
-        return 'Я прыгнул на ' + a * 100 + ' сантиметров';
-    }  
-    
-    if (Array.isArray(a) === 'true') {
-        var sum = 0;
-        var i;
-        for (i = 0; i < a.length; i++) {
-        sum += a[i];    
+  if (typeof(a) === 'number') {
+    return 'Я прыгнул на ' + a * 100 + ' сантиметров';
+  }
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    var length = 0;
+    var s;
+    for (s = 0; s < a.length; s++) {
+    length += a[s] + b[s];
     }
-        return 'Я прошёл ' + sum + ' шагов';
+    return 'Я прошёл ' + length + ' метров';
+  }
+
+  if (Array.isArray(a)) {
+    var sum = 0;
+    var i;
+    for (i = 0; i < a.length; i++) {
+    sum += a[i];
     }
-    
-    if (Array.isArray(a) && Array.isArray(b) === 'true') {
-        var length = 0;
-        var s;
-        for (s = 0; s < a.length; s++) {
-        length += a[s] + b[s];    
-    }
-        return 'Я прошёл ' + length + ' метров';
-    }
+    return 'Я прошёл ' + sum + ' шагов';
+  }
 }
